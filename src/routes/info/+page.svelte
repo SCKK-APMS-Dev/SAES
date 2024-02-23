62eb2fabde5525f8adc39971e55fd43456f0480a
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-23 18:34:19 +0100

    info oldal

---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,12 @@
+<div class="h-screen flex">
+	<div class="m-auto text-white text-center">
+		<h1 class="text-3xl font-bold">Mi ez?</h1>
+		<h2>
+			Ez a See City Közlekedési Központ A műszakának vezetése által létrehozott felület amely
+			megkönnyíti a műszak mögötti papírmunkát a sofőrök és a vezetés részére is egyaránt.
+		</h2>
+		<h2 class="text-gray-300 font-bold">
+			Ennek sem a SeeMTA sem az SCKK vezetőségének nincsen köze.
+		</h2>
+	</div>
+</div>
--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -5,7 +5,7 @@
 <nav class="bg-red-700 flex text-white justify-around items-center py-2 flex-col md:flex-row">
 	<div class="flex items-center gap-2">
 		<img src="/favicon.png" class="pointer-events-none" width="40" height="40" alt="SCKK Logó" />
-		<h1 class="font-bold text-3xl">SCKK</h1>
+		<a href="info" class="font-bold text-3xl">SCKK</a>
 	</div>
 	<div
 		class="flex gap-2 text-xl flex-col text-center md:flex-row md:gap-10 child:w-screen child:md:w-auto child:border-y-2 child:md:border-none"
