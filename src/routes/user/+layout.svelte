5b027503c0564c4fa62e8b8dfd5c638823b08d2c
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-23 18:36:06 +0100

    dizájn

--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -4,11 +4,17 @@
 
 <nav class="bg-red-700 flex text-white justify-around items-center py-2 flex-col md:flex-row">
 	<div class="flex items-center gap-2">
-		<img src="/favicon.png" class="pointer-events-none" width="40" height="40" alt="SCKK Logó" />
-		<a href="info" class="font-bold text-3xl">SCKK</a>
+		<img
+			src="/favicon.png"
+			class="pointer-events-none drop-shadow-xl"
+			width="40"
+			height="40"
+			alt="SCKK Logó"
+		/>
+		<a href="info" class="font-bold text-3xl drop-shadow-xl">SCKK</a>
 	</div>
 	<div
-		class="flex gap-2 text-xl flex-col text-center md:flex-row md:gap-10 child:w-screen child:md:w-auto child:border-y-2 child:md:border-none"
+		class="flex gap-2 text-xl flex-col text-center md:flex-row md:gap-10 child:w-screen child:md:w-auto child:border-y-2 child:md:border-none child:drop-shadow-xl"
 	>
 		<a href="/user" class="hover:font-bold duration-200 transition-all">Kezdőlap</a>
 		<a href="/user" class="hover:font-bold duration-200 transition-all">Pótlékok</a>
