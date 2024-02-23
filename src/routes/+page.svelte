c211831d3996ea61776327f2a5f2814d2d633ed1
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-23 18:13:28 +0100

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 		{#if data?.noauth}
 			<h1 class="text-white text-3xl font-bold">Kérlek lépj be</h1>
 			<a
-				href="auth"
+				href="user"
 				class="text-white bg-green-500 text-2xl font-bold px-3 rounded-xl mt-2 hover:bg-green-800 transition-all duration-200"
 				>Belépés</a
 			>
