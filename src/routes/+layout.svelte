9798556ce9e5f4fa27a9de4fa29c87efe98bce34
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-24 15:57:03 +0100

    töltés anim

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.css';
+	import { navigating } from '$app/stores';
+
+	import { loading } from '$lib/loading';
+
+	$: $loading = !!$navigating;
 </script>
 
 <head>
@@ -11,3 +16,11 @@
 <h2 class="sticky float-right text-red-600 text-center w-screen font-bold">
 	Jelenleg az oldal még fejlesztés alatt áll, nem minden funkció elérhető.
 </h2>
+
+{#if $loading}
+	<div
+		class="absolute bottom-1/2 left-1/2 transform rounded-3xl -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-emerald-500 text-center justify-center items-center flex"
+	>
+		<span class="loader"></span>
+	</div>
+{/if}
