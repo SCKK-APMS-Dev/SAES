542b1647dd1c7e5b72582dca54b2ec430539b88e
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-24 01:56:13 +0100

    azért a discordom fontos

--- src/lib/server/discord.ts
+++ src/lib/server/discord.ts
@@ -1,8 +1,9 @@
 import DiscordOauth2 from 'discord-oauth2';
+import 'dotenv/config';
 
 export const oauth = new DiscordOauth2({
-	clientId: '1210268158792048690',
-	clientSecret: '86bpca8walfcKIj1J9uF3XqnWwSndlTS',
+	clientId: process.env.DISCORD_ID,
+	clientSecret: process.env.DISCORD_SECRET,
 	redirectUri:
 		process.env.NODE_ENV === 'development' ? 'http://localhost:5173/cb' : 'https://sckk.ampix.hu/cb'
 });
