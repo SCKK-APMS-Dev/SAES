f22a1edc9b0eb56ce9b9d74037400bbe50d1bec7
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-25 17:26:14 +0100

    aha

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <div class="h-screen flex">
 	<div class="m-auto text-center">
 		{#if data?.noauth}
-			<h1 class="text-white text-3xl font-bold">Kérlek lépj be</h1>
+			<h1 class="text-white text-3xl font-bold">Az oldal használatához kérlek lépj be</h1>
 			<a
 				href="user"
 				class="text-white bg-green-500 text-2xl font-bold px-3 rounded-xl mt-2 hover:bg-green-800 transition-all duration-200"
