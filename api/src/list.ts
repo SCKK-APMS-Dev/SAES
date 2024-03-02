4d6b2b9aefad94122a0fbe5cddacb16ce19d0700
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-03-02 01:30:58 +0100

    apifix

--- api/src/list.ts
+++ api/src/list.ts
@@ -7,7 +7,8 @@ router.get('/:name/:type', async (req, res) => {
 	const kepek = await prisma.data.findMany({
 		where: {
 			owner: req.params.name.replace('_', ' '),
-			type: req.params.type
+			type: req.params.type,
+			status: 'elfogadva'
 		},
 		select: {
 			date: true,
