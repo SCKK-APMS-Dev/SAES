50c6e1cbe14bddda43b0f8b443237bd471255958
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-03-13 22:45:23 +0100

    buildscriptek

---
+++ scripts/buildapi.bat
@@ -0,0 +1,2 @@
+docker build . -t ghcr.io/sckk-apms-dev/taxiweb-api:latest -f .\Dockerfile.api
+docker push ghcr.io/sckk-apms-dev/taxiweb-api:latest
---
+++ scripts/buildweb.bat
@@ -0,0 +1,2 @@
+docker build . -t ghcr.io/sckk-apms-dev/taxiweb-web:latest -f .\Dockerfile.web
+docker push ghcr.io/sckk-apms-dev/taxiweb-web:latest
