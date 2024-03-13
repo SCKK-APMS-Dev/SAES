597e2bb705d466b405a4685d4b10a0252ae9c777
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-03-13 21:52:48 +0100

    bug fix

--- web/src/routes/user/+layout.svelte
+++ web/src/routes/user/+layout.svelte
@@ -30,7 +30,7 @@
 		<a href="/info" class="font-bold text-3xl drop-shadow-xl">SCKK</a>
 	</div>
 	<button
-		class="cursor-pointer self-center justify-self-end text-3xl font-semibold hover:text-red-500 duration-200 transition-all lg:hidden mr-[10vw]"
+		class="cursor-pointer self-center justify-self-end text-3xl font-semibold hover:text-blue-500 duration-200 transition-all lg:hidden mr-[10vw]"
 		on:click={tognav}
 	>
 		<MaterialSymbolsMenu />
@@ -39,15 +39,16 @@
 		bind:this={nav}
 		class="gap-2 text-xl col-span-2 lg:col-span-1 flex-col lg:!flex items-center hidden text-center lg:mr-[10vw] lg:flex-row lg:gap-5 xl:gap-10 2xl:gap-20 lg:z-auto child:drop-shadow-xl"
 	>
-		<a href="/user" class="hover:text-red-500 duration-200 transition-all">Kezdőlap</a>
-		<a href="/user/help" class="hover:text-red-500 duration-200 transition-all">Segédlet</a>
-		<a href="/user/potlekok" class="hover:text-red-500 duration-200 transition-all">Pótlékok</a>
-		<a href="/user/leintesek" class="hover:text-red-500 duration-200 transition-all">Leintések</a>
-		<a href="/user/szamlak" class="hover:text-red-500 duration-200 transition-all"
+		<a href="/user" class="hover:text-blue-500 duration-200 transition-all">Kezdőlap</a>
+		<a href="/user/help" class="hover:text-blue-500 duration-200 transition-all">Segédlet</a>
+		<a href="/user/potlekok" class="hover:text-blue-500 duration-200 transition-all">Pótlékok</a>
+		<a href="/user/leintesek" class="hover:text-blue-500 duration-200 transition-all">Leintések</a>
+		<a href="/user/szamlak" class="hover:text-blue-500 duration-200 transition-all"
 			>Szereltetési számlák</a
 		>
 		{#if data.layout?.admin}
-			<a href="/user/admin" class="hover:text-red-500 duration-200 transition-all">Műszakvezetés</a>
+			<a href="/user/admin" class="hover:text-blue-500 duration-200 transition-all">Műszakvezetés</a
+			>
 		{/if}
 	</div>
 </nav>
