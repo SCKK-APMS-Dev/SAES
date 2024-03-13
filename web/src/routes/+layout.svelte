28b73114b0dff7d15c52425d108db07748c17735
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-03-14 00:24:33 +0100

    colorteszt

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -15,7 +15,7 @@
 
 {#if $loading}
 	<div
-		class="absolute bottom-1/2 left-1/2 transform rounded-3xl -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-emerald-500 text-center justify-center items-center flex"
+		class="absolute bottom-1/2 left-1/2 transform rounded-3xl -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-slate-500 text-center justify-center items-center flex"
 	>
 		<span class="loader"></span>
 	</div>
