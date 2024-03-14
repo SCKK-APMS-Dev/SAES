2030d693d993e79a18b65b3f141d26a0e6b61fa8
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-03-14 20:00:45 +0100

    fixed potencial error for what I don't know

--- web/src/routes/user/+page.svelte
+++ web/src/routes/user/+page.svelte
@@ -7,9 +7,9 @@
 		<div class="bg-green-600">
 			<h1 class="text-2xl md:text-5xl md:font-bold md:mb-4">Üdv {data.layout?.name}!</h1>
 			<h2 class="text-xl md:text-2xl">
-				Hívásaid (app+leintés): {data.calls.app}+{data.calls.leintes.elfogadott}={Number(
+				Hívásaid (app+leintés): {data.calls?.app}+{data.calls?.leintes.elfogadott}={Number(
 					data.calls.app
-				) + Number(data.calls.leintes.elfogadott)}
+				) + Number(data.calls?.leintes.elfogadott)}
 			</h2>
 			<h2 class="text-xl md:text-2xl">
 				Elfogadott pótlékaid (délelőtti+éjszakai): {data.calls?.potlek.de}+{data.calls?.potlek.éj}
