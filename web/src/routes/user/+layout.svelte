c875e470a7cd89a3433173b89694e5f3f5052153
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-03-21 17:22:38 +0100

    új nav

--- web/src/routes/user/+layout.svelte
+++ web/src/routes/user/+layout.svelte
@@ -18,7 +18,7 @@
 		>segédletet</a
 	>!
 </h2>
-<nav class="bg-gray-700 grid grid-cols-2 lg:flex text-white justify-between items-center py-2">
+<nav class="bg-gray-700 grid lg:flex grid-cols-2 text-white justify-between items-center py-2">
 	<div class="flex items-center gap-2 ml-[10vw] align-middle content-center">
 		<img
 			src="/favicon.png"
@@ -27,35 +27,33 @@
 			height="40"
 			alt="SCKK Logó"
 		/>
-		<a href="/info" class="font-bold text-3xl drop-shadow-xl">SCKK</a>
+		<h1 class="font-bold text-3xl drop-shadow-xl">SCKK</h1>
 	</div>
+	<button
+		class="cursor-pointer self-center justify-self-end text-3xl font-semibold hover:text-blue-500 duration-200 transition-all lg:hidden mr-[10vw]"
+		on:click={tognav}
+	>
+		<MaterialSymbolsMenu />
+	</button>
 	<div
 		bind:this={nav}
-		class="gap-2 z-10 text-xl col-span-2 lg:col-span-1 flex-col flex items-center text-center mr-[10vw] lg:flex-row lg:gap-5 xl:gap-10 2xl:gap-20 lg:z-auto child:drop-shadow-xl"
+		class="gap-2 z-10 text-xl hidden col-span-2 flex-row lg:flex items-center text-center lg:mr-[10vw] lg:flex-row lg:gap-5 xl:gap-10 2xl:gap-20 lg:z-auto child:drop-shadow-xl"
 	>
-		<div class="relative w-full group z-10">
-			<button class="hover:text-blue-500 duration-200 transition-all text-3xl peer"
-				><MaterialSymbolsMenu /></button
+		<div
+			class="relative items-center w-full group z-10 child:bg-gray-700 child:px-2 child:rounded-lg"
+		>
+			<a href="/user" class="hover:bg-slate-600 duration-200 transition-all">Kezdőlap</a>
+			<a href="/user/help" class="hover:bg-slate-600 duration-200 transition-all">Segédlet</a>
+			<a href="/user/potlekok" class="hover:bg-slate-600 duration-200 transition-all">Pótlékok</a>
+			<a href="/user/leintesek" class="hover:bg-slate-600 duration-200 transition-all">Leintések</a>
+			<a href="/user/szamlak" class="hover:bg-slate-600 duration-200 transition-all"
+				>Szereltetési számlák</a
 			>
-			<div
-				class="absolute z-[99] flex flex-col top-[100%] left-[50%] translate-x-[-50%] rounded-md overflow-hidden shadow-lg min-w-[200px] w-max peer-focus:visible peer-focus:opacity-100 opacity-0 invisible duration-200 p-1 bg-gray-100 dark:bg-slate-800 border border-dimmed text-lg md:text-lg"
-			>
-				<a href="/user" class="hover:text-blue-500 duration-200 transition-all">Kezdőlap</a>
-				<a href="/user/help" class="hover:text-blue-500 duration-200 transition-all">Segédlet</a>
-				<a href="/user/potlekok" class="hover:text-blue-500 duration-200 transition-all">Pótlékok</a
+			{#if data.layout?.admin}
+				<a href="/user/admin" class="hover:bg-slate-600 duration-200 transition-all"
+					>Műszakvezetés</a
 				>
-				<a href="/user/leintesek" class="hover:text-blue-500 duration-200 transition-all"
-					>Leintések</a
-				>
-				<a href="/user/szamlak" class="hover:text-blue-500 duration-200 transition-all"
-					>Szereltetési számlák</a
-				>
-				{#if data.layout?.admin}
-					<a href="/user/admin" class="hover:text-blue-500 duration-200 transition-all"
-						>Műszakvezetés</a
-					>
-				{/if}
-			</div>
+			{/if}
 		</div>
 	</div>
 </nav>
--- web/src/routes/user/admin/+layout.svelte
+++ web/src/routes/user/admin/+layout.svelte
@@ -14,7 +14,7 @@
 
 <nav class="bg-emerald-700 grid grid-cols-2 lg:flex text-white justify-between items-center">
 	<div class="flex items-center gap-2 ml-[10vw]">
-		<h1 class="font-bold text-3xl drop-shadow-xl">Műszakvezetés</h1>
+		<h1 class="font-bold text-3xl drop-shadow-xl">Admin</h1>
 	</div>
 	<button
 		class="cursor-pointer self-center justify-self-end text-3xl font-semibold hover:text-emerald-500 duration-200 transition-all lg:hidden mr-[10vw]"
