7ba31d3179b10950a648593902a9b44eef1b27c1
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-03-31 21:41:04 +0200

    new help page

--- web/src/routes/user/help/+page.svelte
+++ web/src/routes/user/help/+page.svelte
@@ -1,37 +1,56 @@
 <div class="text-center text-white">
 	<h1 class="text-3xl font-bold">Segítség az oldal használatához</h1>
-	<h2 class="text-2xl font-bold mt-3">Alapvető információk</h2>
-	<ul class="list-disc list-inside pl-[revert]">
-		<li>Kérlek mindent töltsd fel ide, a taxi-log Discord csatornát nem kell használni</li>
-	</ul>
-	<h2 class="text-2xl font-bold mt-3">Pótlékok</h2>
-	<ul class="list-disc list-inside pl-[revert]">
-		<li>Csak töltsd fel a képet a nyugtáról!</li>
-	</ul>
-	<h2 class="text-2xl font-bold mt-3">Leintések</h2>
-	<ul class="list-disc list-inside pl-[revert]">
-		<li>
-			A leintéseknél két képet kell feltölteni: az 1. képen látszódjon a 10-12 rádiózása, a 2. képen
-			fent, hogy xy kifizette az utazást. Ha nem így fotózod le nem tudjuk elfogadni!
-		</li>
-		<li>
-			Ha egyszerre több leintést töltesz fel (tehát 2-nél több kép) akkor ügyelj a sorrendre!
-			(Érdemes a fájl választóban visszafelé kiválasztani a képet, különben rosszul lesznek
-			párosítva. pl.: ha a kiválasztok fájloknál így írja:
-			"fájl-1.png","fájl-2.png","fájl-3.png","fájl-4.png" akkor az 1-2 és a 3-4 lesz párban!)
-		</li>
-	</ul>
-	<h2 class="text-2xl font-bold mt-3">Számlák</h2>
-	<ul class="list-disc list-inside pl-[revert]">
-		<li>Csak fel kell tölteni, elfogadás után a megjegyzésben tároljuk a számlád értékét.</li>
-	</ul>
-	<h2 class="text-2xl font-bold mt-9">Az oldallal kapcsolatos tervek:</h2>
-	<h2 class="mb-2 text-gray-400">(ezek várhatóak a következő időszakban)</h2>
-	<ul class="list-disc list-inside pl-[revert]">
-		<li>
-			Leintésnél a párok kiválaszthatóak, tehát nem kell sorrendben feltölteni (március 2-3. hete)
-		</li>
-		<li>Kezdőlap működése (március 2-3. hete)</li>
-		<li>Új dizájn (tervben, nincs időpont)</li>
-	</ul>
+	<h2 class="text-2xl font-bold mt-3">Alap információk</h2>
+	<h2 class="text-gray-300 mb-3">
+		Ez a weboldal a taxi és a vontatósok papírmunkáját könnyíti meg. Ide kell feltölteni a
+		leintéseket, számlákat illetve a műszakon kívüli pótlékokat.
+	</h2>
+	<div class="align-bottom">
+		<h1 class="text-3xl font-bold text-left ml-[20%] mt-5">1. Pótlékok</h1>
+		<ul class="child:mx-[20%]">
+			<li class="flex items-center gap-2">
+				<h2 class="text-xl text-left font-bold">1.1.</h2>
+				<h2 class="text-xl">A fotónak tartalmaznia kell a nyugtát.</h2>
+			</li>
+			<li class="flex items-center gap-2">
+				<h2 class="text-xl text-left font-bold">1.2.</h2>
+				<h2 class="text-xl">
+					Pótlékok a következő időben érvényesek: 8-15 óra (délelőtti), 22-4 (éjszakai)
+				</h2>
+			</li>
+		</ul>
+		<h1 class="text-3xl font-bold text-left ml-[20%] mt-5">2. Leintések</h1>
+		<ul class="child:mx-[20%] child:text-left">
+			<li class="flex gap-2">
+				<h2 class="text-xl font-bold">2.1.</h2>
+				<h2 class="text-xl">Egy érvényes leintés 2 képből áll</h2>
+			</li>
+			<li class="flex gap-2">
+				<h2 class="text-xl font-bold">2.2.</h2>
+				<h2 class="text-xl">
+					Az első képen a 10-12 rádiózásának, vagy hívás átvételnél a rádió rp-nek kell látszódnia
+				</h2>
+			</li>
+			<li class="flex gap-2">
+				<h2 class="text-xl font-bold">2.3.</h2>
+				<h2 class="text-xl">
+					A második képen a közép fent található "xy kifizette az utazást" szövegnek kell látszódnia
+					vagy a kék dobozban, vagy a chaten
+				</h2>
+			</li>
+		</ul>
+		<h1 class="text-3xl font-bold text-left ml-[20%] mt-5">3. Számlák</h1>
+		<ul class="child:mx-[20%] child:text-left">
+			<li class="flex gap-2">
+				<h2 class="text-xl font-bold">3.1.</h2>
+				<h2 class="text-xl">
+					Csak is saját (te vagy az ügyfél a számlán) szereléseket tölthetsz fel, autó függetlenül
+				</h2>
+			</li>
+			<li class="flex gap-2">
+				<h2 class="text-xl font-bold">2.2.</h2>
+				<h2 class="text-xl">Csak az APMS általi szerelésekért tudjuk visszaadni a pénzt</h2>
+			</li>
+		</ul>
+	</div>
 </div>
