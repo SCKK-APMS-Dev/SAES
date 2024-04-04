7096c1f69be49a8b327af3214ef5d233a3a2a8ff
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-04-04 19:22:56 +0200

    changed am pótlék time

--- web/src/routes/user/potlekok/upload/+page.svelte
+++ web/src/routes/user/potlekok/upload/+page.svelte
@@ -2,13 +2,11 @@
 	import Upload from '$lib/upload.svelte';
 	import Error from '$lib/error.svelte';
 	export let data;
+	let text = data.layout.am
+		? '8:00-16:00: Délelőtti pótlék, 20:00-4:00: Éjszakai pótlék'
+		: '8:00-15:00: Délelőtti pótlék, 22:00-4:00: Éjszakai pótlék';
 </script>
 
 <Error {data}>
-	<Upload
-		adat={data}
-		title="Pótlék"
-		underhood="pótlék"
-		desc="8:00-15:00: Délelőtti pótlék, 22:00-4:00: Éjszakai pótlék"
-	/>
+	<Upload adat={data} title="Pótlék" underhood="pótlék" desc={text} />
 </Error>
