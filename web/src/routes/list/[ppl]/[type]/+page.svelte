0feb3f526fccc29468344c08263df82dd4bc4eed
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-04-05 22:53:53 +0200

    fixleintés kép mert hülye vagyok

--- web/src/routes/list/[ppl]/[type]/+page.svelte
+++ web/src/routes/list/[ppl]/[type]/+page.svelte
@@ -6,8 +6,8 @@
 {#each data.cucc as puszedli}
 	<h2 class="text-xl text-white text-center mt-5">{data.cucc.indexOf(puszedli) + 1}:</h2>
 	{#if data.type === 'leintés'}
+		<img src={`https://api.sckk.hu/img/data/${puszedli.id}/0`} alt="kép" />
 		<img src={`https://api.sckk.hu/img/data/${puszedli.id}/1`} alt="kép" />
-		<img src={`https://api.sckk.hu/img/data/${puszedli.id}/2`} alt="kép" />
 	{:else}
 		<img src={`https://api.sckk.hu/img/data/${puszedli.id}`} alt="kép" />
 	{/if}
