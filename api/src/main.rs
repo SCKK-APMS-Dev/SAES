594de2178a38e05a8c4cc6cd14460b57ac150978
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-04-19 22:23:29 +0200

    aha nem

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,9 +1,19 @@
+use std::env;
+
+use dotenv::dotenv;
+
+use sqlx::mysql::MySqlConnection;
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+#[test]
+fn test() {
+    dotenv().ok();
+    println!("Argument: {}", env::var("DATABASE_URL").unwrap());
 }
 
 #[launch]
