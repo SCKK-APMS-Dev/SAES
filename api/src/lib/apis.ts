a00e71cf887e2205ef1c10e20649fa349c7cd640
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-04-30 18:18:27 +0200

    new ip

--- api/src/lib/apis.ts
+++ api/src/lib/apis.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 export function getApiUrl(api: 'erik' | 'patrik') {
 	if (api === 'erik') {
-		return process.env.NODE_DEV ? 'https://app.sckk.hu' : 'http://192.168.100.138:5472';
+		return 'https://app.sckk.hu';
 	}
 	if (api === 'patrik') {
 		return process.env.NODE_DEV
