34e5ac65882bdf1490a405c3c6deb6486ffc987d
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-05-11 17:01:11 +0200

    calls whitelist

--- api/src/user/calls.rs
+++ api/src/user/calls.rs
@@ -1,9 +1,17 @@
 use axum::{debug_handler, extract::Request, Json};
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
 
 use crate::cucc::{api::get_api_envs, middle::Tag};
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct DriverRecord {
+    pub driver: String,
+    pub count: u32,
+}
+
 #[debug_handler]
-pub async fn calls(mut request: Request) -> String {
+pub async fn calls(mut request: Request) -> Json<DriverRecord> {
     let client = reqwest::Client::new();
     let envs = get_api_envs();
     let calls = client
@@ -14,5 +22,13 @@ pub async fn calls(mut request: Request) -> String {
         .text()
         .await
         .expect("Átalakítás sikertelen");
-    calls
+    let driver_records: Vec<DriverRecord> = from_str(&calls).expect("Átalakítás nem megyen");
+
+    Json(
+        driver_records
+            .iter()
+            .find(|record| record.driver == "Lemondott")
+            .unwrap()
+            .clone(),
+    )
 }
