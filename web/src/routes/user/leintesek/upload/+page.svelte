3f981dbcee02973c373502382c3af40de9fba529
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-06-05 10:28:08 +0200

    takarítás

--- web/src/routes/user/leintesek/upload/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Upload from '$lib/upload.svelte';
-	export let data;
-	import Error from '$lib/error.svelte';
-</script>
-
-<Error {data}>
-	<Upload adat={data} title="Leintés" underhood="leintés" />
-</Error>
--- web/src/routes/user/szamlak/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let data;
-	import List from '$lib/list.svelte';
-	import Error from '$lib/error.svelte';
-</script>
-
-<Error {data}>
-	<List {data} magazo="Számláid" url="szamlak" />
-</Error>
