f9360cbfa1b10ee79d1f4e21a8611a66feb5d793
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-06-23 23:14:12 +0200

    a

--- buildweb.bat
+++ buildweb.bat
@@ -1,3 +1,3 @@
 cd web
-@REM docker build --platform linux/amd64,linux/arm64 . -t ghcr.io/sckk-apms-dev/taxiweb-web:latest
+@REM docker build --platform linux/amd64,linux/arm64 . -t ghcr.io/sckk-apms-dev/taxiweb-web:latest as
 docker build --platform linux/amd64 . -t ghcr.io/sckk-apms-dev/taxiweb-web:latest --push
