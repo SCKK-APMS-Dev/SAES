be0996479ed1edb566e930444a6f1f31365c73c0
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-06-26 21:36:18 +0200

    working img api

--- api/src/image.rs
+++ api/src/image.rs
@@ -1,9 +1,4 @@
-use axum::{
-    body::Body,
-    debug_handler,
-    extract::Query,
-    response::{IntoResponse, Response},
-};
+use axum::{body::Body, debug_handler, extract::Query, response::Response};
 use reqwest::StatusCode;
 use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use tokio::{fs::File, io::AsyncReadExt};
@@ -14,7 +9,7 @@ use crate::{
 };
 
 #[debug_handler]
-pub async fn image_get(cucc: Query<ImgHeader>) -> Result<impl IntoResponse, StatusCode> {
+pub async fn image_get(cucc: Query<ImgHeader>) -> Response {
     let db = get_conn().await;
     let kep = Data::Entity::find()
         .filter(Data::Column::Id.eq(cucc.id.clone()))
@@ -26,10 +21,17 @@ pub async fn image_get(cucc: Query<ImgHeader>) -> Result<impl IntoResponse, Stat
         let mut kep_file = File::open(format!("./public/{}", fel_kep.kep))
             .await
             .unwrap();
-        let mut content = Vec::new();
-        kep_file.read_to_end(&mut content);
-        Ok((content))
+        let mut contents = vec![];
+        let _ = kep_file.read_to_end(&mut contents).await;
+        let body = Body::from(contents);
+        Response::builder()
+            .header("Content-Type", "image/png")
+            .body(body)
+            .unwrap()
     } else {
-        Err(StatusCode::NOT_FOUND)
+        Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::empty())
+            .unwrap()
     }
 }
