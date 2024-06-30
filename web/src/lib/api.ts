a3701585a25a7a0ee9d7c685c58d12e6fe382f6e
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-06-30 21:11:57 +0200

    fixed url

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,2 +1,2 @@
 export const apiUrl =
-	process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api.sckk.hu';
+	process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api-teszt.sckk.hu';
