88849e086d4909bece10baa68c4ccbde77206b5c
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-07-23 22:37:49 +0200

    added google bot to monorepo

--- build/web.bat
+++ build/web.bat
@@ -1,3 +1,3 @@
 cd web
 @REM docker build --platform linux/amd64,linux/arm64 . -t ghcr.io/sckk-apms-dev/taxiweb-web:latest
-docker build --platform linux/amd64 . -t ghcr.io/sckk-apms-dev/taxiweb-web:test --push
+docker build --platform linux/amd64 . -t ghcr.io/sckk-apms-dev/sckkextra-web:test --push
