4ee3c8983afffc594ca69080380a21062cbbeb78
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-07-31 03:26:11 +0200

    added multipage for items listing

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -16,11 +16,13 @@
 <slot />
 
 {#if $loading}
-	<div
-		class="shadow-taxi border-taxi pointer-events-none absolute
- left-1/2 top-1/2 h-96 w-96 -translate-x-1/2 -translate-y-1/2 transform cursor-progress content-center overflow-hidden rounded-full border-2 shadow-lg"
-	>
-		<!-- <span class="loader"></span> -->
-		<img src="/macska.gif" alt="loading" class="block w-[1000px] object-top" />
+	<div class="fixed top-0 z-50 h-full w-full bg-[rgba(0,0,0,0.4)]">
+		<div
+			class="shadow-taxi border-taxi pointer-events-none fixed
+	left-1/2 top-1/2 h-96 w-96 -translate-x-1/2 -translate-y-1/2 transform cursor-progress content-center overflow-hidden rounded-full border-2 shadow-lg"
+		>
+			<!-- <span class="loader"></span> -->
+			<img src="/macska.gif" alt="loading" class=" z-50 block w-[1000px] object-top" />
+		</div>
 	</div>
 {/if}
