72a5cc5d83e33c4d739b7f039533ef6b429984ae
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-08-02 13:18:38 +0200

    text

--- web/src/routes/user/admin/+page.svelte
+++ web/src/routes/user/admin/+page.svelte
@@ -1,5 +1,12 @@
-<h1
-	class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-3xl font-bold text-white"
->
-	Hamarosan! 😉
-</h1>
+<div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center">
+	<h1 class=" mb-2 text-3xl font-bold text-white">Hamarosan! 😉</h1>
+	<h2 class="text-gray-400">
+		Eddig, a Műszakvezetés gomb az eheti statisztikához vitt, most viszont az ún. Főoldalra.
+	</h2>
+	<h2 class="text-gray-400">
+		A Főoldalon majd egy fokkal másabb statisztikát láthattok, erről majd később.
+	</h2>
+	<h2 class="text-xl font-bold text-gray-400">
+		A Főoldalra vonatkozik a hamarosan, nem a Műszakvezetésre. 🙂
+	</h2>
+</div>
