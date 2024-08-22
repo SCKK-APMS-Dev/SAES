78b0f489229a93e85aa44ee4dea1c8ae33a68c6d
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-08-22 23:11:26 +0200

    datefix

--- web/src/routes/ucp/potlekok/upload/+page.svelte
+++ web/src/routes/ucp/potlekok/upload/+page.svelte
@@ -10,5 +10,5 @@
 	tipus="pótlék"
 	warning={data.layout.am
 		? '8:00-16:00: Délelőtti pótlék, 20:00-4:00: Éjszakai pótlék'
-		: '8:00-15:00: Délelőtti pótlék, 22:00-4:00: Éjszakai pótlék'}
+		: '8:00-14:00: Délelőtti pótlék, 23:00-4:00: Éjszakai pótlék'}
 />
