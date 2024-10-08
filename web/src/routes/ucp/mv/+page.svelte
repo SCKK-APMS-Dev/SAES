2a43acc8bc9e4c779b7fc9ab293466ba017dc098
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-10-08 23:30:53 +0200

    multi-lang stat page

--- web/src/routes/ucp/mv/+page.svelte
+++ web/src/routes/ucp/mv/+page.svelte
@@ -3,6 +3,7 @@
 	import { socket } from '$lib/socket';
 	export let data;
 	let count = 0;
+	let color: string = 'ro';
 	$socket?.emit('JoinEvent', { event_name: 'socketppl' });
 	$socket?.on('socketppl-update', (data) => {
 		count = data;
@@ -16,39 +17,89 @@
 	<h1 class="mb-2 text-3xl font-bold text-white">Műszakvezetési statisztika</h1>
 	<h2 class="mb-2 mt-5 text-xl text-white">Weboldal használók száma: {count}</h2>
 	<div class="child:p-2 md:child:p-4 ml-5 mr-5 grid grid-cols-3 gap-5 text-center text-white">
-		<div class="rounded-lg bg-red-600">
+		<div
+			class="rounded-lg"
+			class:bg-red-700={color === 'hu'}
+			class:bg-white={color === 'sk'}
+			class:text-black={color === 'sk'}
+			class:bg-blue-900={color === 'ro'}
+		>
 			<h1 class="text-2xl font-bold">Új pótlékok száma:</h1>
 			<h2 class="text-3xl font-bold">{data.stat.potlek.feltoltve}</h2>
 		</div>
-		<div class="rounded-lg bg-red-600">
+		<div
+			class="rounded-lg"
+			class:bg-red-700={color === 'hu'}
+			class:bg-white={color === 'sk'}
+			class:text-black={color === 'sk'}
+			class:bg-yellow-400={color === 'ro'}
+		>
 			<h1 class="text-2xl font-bold">Új leintések száma:</h1>
 			<h2 class="text-3xl font-bold">{data.stat.leintes.feltoltve}</h2>
 		</div>
-		<div class="rounded-lg bg-red-600">
+		<div
+			class="rounded-lg"
+			class:bg-red-700={color === 'hu' || color === 'ro'}
+			class:bg-white={color === 'sk'}
+			class:text-black={color === 'sk'}
+		>
 			<h1 class="text-2xl font-bold">Új számlák száma:</h1>
 			<h2 class="text-3xl font-bold">{data.stat.szamla.feltoltve}</h2>
 		</div>
-		<div class="rounded-lg bg-white text-black">
+		<div
+			class="rounded-lg"
+			class:bg-white={color === 'hu'}
+			class:text-black={color === 'hu'}
+			class:bg-blue-700={color === 'sk'}
+			class:bg-blue-900={color === 'ro'}
+		>
 			<h1 class="text-2xl font-bold">Elfogadott pótlékok száma:</h1>
 			<h2 class="text-3xl font-bold">{data.stat.potlek.elfogadva}</h2>
 		</div>
-		<div class="rounded-lg bg-white text-black">
+		<div
+			class="rounded-lg"
+			class:bg-white={color === 'hu'}
+			class:text-black={color === 'hu'}
+			class:bg-blue-700={color === 'sk'}
+			class:bg-yellow-400={color === 'ro'}
+		>
 			<h1 class="text-2xl font-bold">Elfogadott leintések száma:</h1>
 			<h2 class="text-3xl font-bold">{data.stat.leintes.elfogadva}</h2>
 		</div>
-		<div class="rounded-lg bg-white text-black">
+		<div
+			class="rounded-lg"
+			class:bg-white={color === 'hu'}
+			class:text-black={color === 'hu'}
+			class:bg-blue-700={color === 'sk'}
+			class:bg-red-700={color === 'ro'}
+		>
 			<h1 class="text-2xl font-bold">Elfogadott számlák száma:</h1>
 			<h2 class="text-3xl font-bold">{data.stat.szamla.elfogadva}</h2>
 		</div>
-		<div class="rounded-lg bg-green-600">
+		<div
+			class="rounded-lg"
+			class:bg-blue-900={color === 'ro'}
+			class:bg-red-600={color === 'sk'}
+			class:bg-green-600={color === 'hu'}
+		>
 			<h1 class="text-2xl font-bold">Elutasított pótlékok száma:</h1>
 			<h2 class="text-3xl font-bold">{data.stat.potlek.elutasitva}</h2>
 		</div>
-		<div class="rounded-lg bg-green-600">
+		<div
+			class="rounded-lg"
+			class:bg-red-600={color === 'sk'}
+			class:bg-yellow-400={color === 'ro'}
+			class:bg-green-600={color === 'hu'}
+		>
 			<h1 class="text-2xl font-bold">Elutasított leintések száma:</h1>
 			<h2 class="text-3xl font-bold">{data.stat.leintes.elutasitva}</h2>
 		</div>
-		<div class="rounded-lg bg-green-600">
+		<div
+			class="rounded-lg"
+			class:bg-red-600={color === 'sk'}
+			class:bg-red-700={color === 'ro'}
+			class:bg-green-600={color === 'hu'}
+		>
 			<h1 class="text-2xl font-bold">Elutasított számlák száma:</h1>
 			<h2 class="text-3xl font-bold">{data.stat.szamla.elutasitva}</h2>
 		</div>
