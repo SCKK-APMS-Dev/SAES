c3b79a0ad803ed036e37097ae5c61b0aab09e0ea
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-10-09 11:30:01 +0200

    log headers

--- web/src/routes/ucp/mv/+page.server.ts
+++ web/src/routes/ucp/mv/+page.server.ts
@@ -1,12 +1,13 @@
 import { apiUrl } from '$lib/api';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ cookies }) => {
+export const load = (async ({ cookies, request }) => {
 	const fetcs = await fetch(`${apiUrl}/ucp/mv/home`, {
 		headers: {
 			cookie: cookies.get('auth_token')!
 		}
 	});
+	console.log(request.headers);
 	return {
 		stat: await fetcs.json()
 	};
