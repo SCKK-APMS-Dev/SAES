06f6ac2e4570733615c96e1276226dcb8fa30c32
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-10-13 00:48:42 +0200

    web: added new item list hover design to leintés aswell

--- web/src/lib/ucp/list.svelte
+++ web/src/lib/ucp/list.svelte
@@ -64,7 +64,7 @@
 						class:from-cyan-800={potle.status === 'feltöltve'}
 						class:to-gray-700={potle.status === 'feltöltve'}
 					>
-						<h1 class="-mb-2 text-2xl font-bold">{potle.status.toUpperCase()}</h1>
+						<h1 class="-mb-2 text-2xl font-bold drop-shadow-xl">{potle.status.toUpperCase()}</h1>
 						<h1 class="text-gray-200 drop-shadow-xl">
 							{new Date(potle.date).getUTCFullYear()}.{new Date(potle.date).getUTCMonth() +
 								1}.{new Date(potle.date).getUTCDate()}. {new Date(potle.date).getUTCHours() +
@@ -76,16 +76,42 @@
 
 						{#if tipus === 'leintés'}
 							<div class="flex flex-col xl:flex-row">
-								<img
-									src={`${data.api}/limg?id=${potle.id}&ver=0`}
-									alt=""
-									class="max-h-xl m-auto max-w-xl py-2 drop-shadow-xl"
-								/>
-								<img
-									src={`${data.api}/limg?id=${potle.id}&ver=1`}
-									alt=""
-									class="max-h-xl m-auto max-w-xl py-2 drop-shadow-xl"
-								/>
+								<a
+									href={`${data.api}/limg?id=${potle.id}&ver=0`}
+									target="_blank"
+									on:mouseenter={() => (potle.focus1 = true)}
+									on:mouseleave={() => (potle.focus1 = false)}
+								>
+									<img
+										src={`${data.api}/limg?id=${potle.id}&ver=0`}
+										alt=""
+										class="max-h-xl m-auto max-w-xl py-2 drop-shadow-xl"
+										class:blur={potle.focus1}
+									/>
+									{#if potle.focus1}
+										<span
+											class="icon-[mdi--gesture-touch-hold] absolute left-1/4 top-1/2 h-24 w-24 -translate-x-1/4 -translate-y-1/2 transform shadow-2xl"
+										></span>
+									{/if}
+								</a>
+								<a
+									href={`${data.api}/limg?id=${potle.id}&ver=1`}
+									target="_blank"
+									on:mouseenter={() => (potle.focus2 = true)}
+									on:mouseleave={() => (potle.focus2 = false)}
+								>
+									<img
+										src={`${data.api}/limg?id=${potle.id}&ver=1`}
+										alt=""
+										class="max-h-xl m-auto max-w-xl py-2 drop-shadow-xl"
+										class:blur={potle.focus2}
+									/>
+									{#if potle.focus2}
+										<span
+											class="icon-[mdi--gesture-touch-hold] absolute left-3/4 top-1/2 h-24 w-24 -translate-x-3/4 -translate-y-1/2 transform shadow-2xl"
+										></span>
+									{/if}
+								</a>
 							</div>
 						{:else}
 							<a
