d43fb5fd2be7e2d6bdd0da7a89973f8abdfa02ae
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-11-01 11:50:38 +0100

    web: updated the list system

--- web/src/routes/list/[ppl]/[type]/+page.svelte
+++ web/src/routes/list/[ppl]/[type]/+page.svelte
@@ -6,10 +6,13 @@
 	{#each data.cucc as puszedli}
 		{#if data.type === 'szamla'}
 			<h2 class="mt-5 text-center text-xl text-white">
-				{data.cucc.indexOf(puszedli) + 1} ({puszedli.extra}$):
+				{data.cucc.indexOf(puszedli) + 1}. ({puszedli.extra}$):
 			</h2>
 		{:else}
 			<h2 class="mt-5 text-center text-xl text-white">{data.cucc.indexOf(puszedli) + 1}:</h2>
+		{/if}
+		{#if puszedli.reason}
+			<h2 class="text-center text-gray-400">Megjegyzés: {puszedli.reason}</h2>
 		{/if}
 		{#if data.type === 'leintes'}
 			<img src={`${data.api}/limg?id=${puszedli.id}&ver=0`} alt="kép" />
@@ -17,5 +20,6 @@
 		{:else}
 			<img src={`${data.api}/img?id=${puszedli.id}`} alt="kép" />
 		{/if}
+		<h2 class="text-gray-400">ID: {puszedli.id}</h2>
 	{/each}
 </div>
