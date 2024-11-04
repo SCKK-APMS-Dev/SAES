1284d17a5f53cbf8b9edac59709f83b756e07e93
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-11-04 17:32:15 +0100

    web: fixed api.ts

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,4 +1,3 @@
-export const apiUrl =
-	process.env.NODE_ENV === 'development'
-		? 'http://localhost:3000'
-		: (process.env.api_prod_url as string);
+export const apiUrl = process.env.NODE_ENV === "development"
+	? "http://localhost:3000/api"
+	: (process.env.api_prod_url as string);
