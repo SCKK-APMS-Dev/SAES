882c5815fed2ca701146c618f26ce60530c16ce4
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-11-28 19:00:59 +0100

    done webn for now

--- webn/src/main.rs
+++ webn/src/main.rs
@@ -1,24 +1,52 @@
+use routes::base::SuperBase;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
+mod routes;
 
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Base,
+    #[at("/secure")]
+    Secure,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+#[function_component(Secure)]
+fn secure() -> Html {
+    let navigator = use_navigator().unwrap();
+
+    let onclick = Callback::from(move |_| navigator.push(&Route::Base));
     html! {
         <div>
-            <button {onclick}>{ "+1" }</button>
-            <p class="font-bold text-3xl">{ *counter }</p>
+            <h1>{ "Secure" }</h1>
+            <button {onclick}>{ "Go Home" }</button>
         </div>
     }
 }
 
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Base => html! { <SuperBase/> },
+        Route::Secure => html! {
+            <Secure />
+        },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
+
+#[function_component(Main)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<Main>::new().render();
 }
--- webn/tailwind.config.js
+++ webn/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,rs}'],
+	content: ['./src/**/*.{html,rs}', 'index.html'],
 	theme: {
 		extend: {}
 	},
