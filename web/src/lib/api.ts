78eb21faca46c29bf1a8697a2a0d93e4cf7f5ff0
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-12-05 00:16:03 +0100

    static typed snow

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -2,8 +2,4 @@ export const apiUrl = process.env.NODE_ENV === "development"
 	? "http://localhost:3000"
 	: (process.env.api_prod_url as string);
 
-export const snow = process.env.NODE_ENV === "development"
-	? false // * DEV SNOW true / false
-	: process.env.snow
-	? true
-	: false;
+export const snow = false;
