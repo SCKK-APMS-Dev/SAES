10dccddbe9b658664bf75d58fabb228fcd4b5101
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-12-05 23:56:55 +0100

    web: enabled snow

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -2,4 +2,4 @@ export const apiUrl = process.env.NODE_ENV === "development"
 	? "http://localhost:3000"
 	: (process.env.api_prod_url as string);
 
-export const snow = false;
+export const snow = true;
