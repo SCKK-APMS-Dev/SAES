3ffdbdfd147eaa55a2a60e95df88fe1bf9ab5368
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-12-31 03:02:38 +0100

    fixed image request + rename

--- web/src/routes/ucp/mv/potlekok/+page.svelte
+++ web/src/routes/ucp/mv/potlekok/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import Adminlist from '$lib/mv/mvlist.svelte';
+	import MVList from '$lib/mv/mvlist.svelte';
 	import { get_type_number } from '$lib/ucp/types.js';
 	let { data } = $props();
 </script>
 
-<Adminlist
+<MVList
 	title="Pótlékok"
 	des={`Taxi: 8-15 Délelőtti, 22-4 Éjszakai; AM: 8-16 Délelőtti, 20-4 Éjszakai`}
 	type={get_type_number('pótlék')}
