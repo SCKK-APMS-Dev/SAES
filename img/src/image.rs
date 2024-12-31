7a4b8246a3e42909f6dac9a8b27ac6e7327fbf4d
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-12-31 02:25:28 +0100

    img server done

---
+++ img/src/image.rs
@@ -0,0 +1,58 @@
+use std::env;
+
+use axum::{body::Body, debug_handler, extract::Query, http::StatusCode, response::Response};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+use serde::Deserialize;
+use tokio::{fs::File, io::AsyncReadExt};
+
+use crate::{db::images as Images, sql::get_db_conn};
+
+#[derive(Debug, Deserialize)]
+pub struct ImgQuery {
+    pub id: i32,
+}
+
+#[debug_handler]
+pub async fn image_get(q: Query<ImgQuery>) -> Response {
+    let db = get_db_conn().await;
+    let parent_dir = env::var("PARENT_IMAGE_DIR").expect("PARENT_IMAGE_DIR env nem létezik");
+    let image = Images::Entity::find()
+        .filter(Images::Column::Id.eq(q.id.clone()))
+        .one(&db)
+        .await
+        .unwrap();
+    if image.is_some() {
+        let fel_image = image.unwrap();
+        let mut image_file = if fel_image.tmp == 0 {
+            File::open(format!("{}/{}", parent_dir, fel_image.filename))
+                .await
+                .expect("[ERROR] Fájl megnyitása sikertelen")
+        } else {
+            File::open(format!("{}/tmp/{}", parent_dir, fel_image.filename))
+                .await
+                .expect("[ERROR] Fájl megnyitása sikertelen")
+        };
+        let mut contents = vec![];
+        let _ = image_file.read_to_end(&mut contents).await;
+        let body = Body::from(contents);
+        Response::builder()
+            .header(
+                "Content-Type",
+                format!(
+                    "image/{}",
+                    if fel_image.converted > 0 {
+                        "avif"
+                    } else {
+                        "png"
+                    }
+                ),
+            )
+            .body(body)
+            .unwrap()
+    } else {
+        Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::empty())
+            .unwrap()
+    }
+}
--- readme.md
+++ readme.md
@@ -5,4 +5,5 @@ Mit tartalmaz ez a monorepo?
 - `web` az SCKK weboldalt, [![SAES Web Build](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-web.yml/badge.svg)](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-web.yml)
 - `api` a SAES api-ját, [![SAES API Build](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-api.yml/badge.svg)](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-api.yml)
 - `gbot` az SCKK hívásrögzítő botját, [![SAES GBot Build](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-gbot.yml/badge.svg)](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-gbot.yml)
+- `img` a SAES image közvetítő szerverét, [![SAES Img Build](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-img.yml/badge.svg)](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-img.yml)
 - `converter` az AVIF kép convertert, [![SAES Converter Build](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-converter.yml/badge.svg)](https://github.com/SCKK-APMS-Dev/SAES/actions/workflows/build-converter.yml)
