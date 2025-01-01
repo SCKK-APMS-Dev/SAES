9ea318e5606f6a15ea1aafb4f1b0047d1e470bdf
HVCsano <55344602+HVCsano@users.noreply.github.com>
2025-01-01 18:26:59 +0100

    fixed broken login

--- web/src/routes/ucp/+layout.svelte
+++ web/src/routes/ucp/+layout.svelte
@@ -9,6 +9,7 @@
 	import { socket } from '$lib/socket.js';
 	import ViewTransition from '$lib/navigation.svelte';
 	import Header from '$lib/ucp/header.svelte';
+	import { apiUrl } from '$lib/api.js';
 	let { data, children } = $props();
 	let maintenance = $state(false);
 	let initial_socket = $state(false);
@@ -56,7 +57,7 @@
 </script>
 
 <svelte:head>
-	{#if !maintenance || data.maintenance || !data.noauth}
+	{#if !maintenance || data.maintenance || data.noauth}
 		{#if !navigating.type}
 			{#if page.url.pathname.includes('mv')}
 				<title>Műszakvezetői felület - {tip}</title>
@@ -105,14 +106,34 @@
 		<main>
 			<div class="flex h-screen">
 				<div class="m-auto text-center">
-					<h2 class="text-center text-3xl font-bold text-white">
-						A weboldal használatához kérlek lépj be
-					</h2>
-					<a
-						href={`${data.apiUrl}/auth?path=${page.url.pathname}`}
-						class="hover:bg-pos-100 bg-size-200 bg-pos-0 to-tow via-taxi mb-5 ml-5 mr-5 mt-5 block rounded-full bg-gradient-to-r from-emerald-500 px-2 py-1 text-center text-lg font-bold text-white drop-shadow-lg transition-all duration-500"
-						>Belépés</a
+					<h1 class="animate-pulse text-3xl font-bold text-black dark:text-white">
+						Az oldal használatához kérlek lépj be
+					</h1>
+					<button
+						class="from-taxi hover:bg-pos-100 bg-size-200 bg-pos-0 group relative m-auto mt-3 flex h-12 animate-bounce items-center space-x-2 overflow-hidden rounded-full bg-gradient-to-r via-rose-500 to-red-600 px-6 transition-all duration-500"
 					>
+						<a href={`${apiUrl}/auth`}>
+							<span
+								class="relative text-xl font-bold text-white transition-colors duration-300 group-hover:text-black"
+								>Belépés Discordal</span
+							>
+						</a>
+						<div class="flex translate-x-3 items-center -space-x-3">
+							<div
+								class="h-[1.6px] w-2.5 origin-left scale-x-0 rounded bg-white transition duration-300 group-hover:scale-x-100 group-hover:bg-black"
+							></div>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-5 w-5 -translate-x-2 stroke-white transition duration-300 group-hover:translate-x-0 group-hover:stroke-black"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								stroke-width="2"
+							>
+								<path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+							</svg>
+						</div>
+					</button>
 				</div>
 			</div>
 		</main>
