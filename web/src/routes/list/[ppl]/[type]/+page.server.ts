9d301f0681b5ba012dc6d1d37d160aca9802c647
HVCsano <55344602+HVCsano@users.noreply.github.com>
2025-01-04 17:09:53 +0100

    fixed image showing

--- web/src/routes/list/[ppl]/[type]/+page.server.ts
+++ web/src/routes/list/[ppl]/[type]/+page.server.ts
@@ -1,9 +1,11 @@
-import { apiUrl } from '$lib/api';
-import type { PageServerLoad } from './$types';
+import { apiUrl, imageUrl } from "$lib/api";
+import type { PageServerLoad } from "./$types";
 
 export const load = (async ({ params }) => {
 	const aha = await fetch(
-		`${apiUrl}/list?driver=${params.ppl.replace('_', ' ')}&tipus=${params.type}`
+		`${apiUrl}/list?driver=${
+			params.ppl.replace("_", " ")
+		}&tipus=${params.type}`,
 	);
 
 	if (aha.ok) {
@@ -11,7 +13,8 @@ export const load = (async ({ params }) => {
 		return {
 			cucc: text,
 			type: params.type,
-			api: apiUrl
+			api: apiUrl,
+			image: imageUrl,
 		};
 	}
 }) satisfies PageServerLoad;
