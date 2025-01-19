a7fb23a56454e2dab559c28617518caa668fcb1d
HVCsano <55344602+HVCsano@users.noreply.github.com>
2025-01-19 11:31:10 +0100

    added tray for the app

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
-use tauri::Manager;
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+    Manager,
+};
 
 #[tauri::command]
 async fn update_done(app_handle: tauri::AppHandle) {
@@ -12,6 +16,29 @@ async fn update_done(app_handle: tauri::AppHandle) {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .setup(|app| {
+            let quit_i = MenuItem::with_id(app, "quit", "Kilépés", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i])?;
+
+            let tray = TrayIconBuilder::new()
+                .menu(&menu)
+                .title("SAMT App")
+                .tooltip("SAMT App")
+                .icon(app.default_window_icon().unwrap().clone())
+                .show_menu_on_left_click(false)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "quit" => {
+                        println!("quit menu item was clicked");
+                        app.exit(0);
+                    }
+                    _ => {
+                        println!("menu item {:?} not handled", event.id);
+                    }
+                })
+                .build(app)?;
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![update_done])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
