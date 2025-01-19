dd4332c5f11186aec45987e10a3716fb57985e82
HVCsano <55344602+HVCsano@users.noreply.github.com>
2025-01-19 01:30:14 +0100

    added app update

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,11 +1,16 @@
 use std::{thread, time::Duration};
 
 use tauri::Manager;
+use tauri_plugin_updater::UpdaterExt;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                update(handle).await.unwrap();
+            });
             let overlay = app.get_webview_window("overlay").unwrap();
             let loader = app.get_webview_window("loader").unwrap();
             let main = app.get_webview_window("main").unwrap();
@@ -18,4 +23,28 @@ pub fn run() {
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+    if let Some(update) = app.updater()?.check().await? {
+        let mut downloaded = 0;
+
+        // alternatively we could also call update.download() and update.install() separately
+        update
+            .download_and_install(
+                |chunk_length, content_length| {
+                    downloaded += chunk_length;
+                    println!("downloaded {downloaded} from {content_length:?}");
+                },
+                || {
+                    println!("download finished");
+                },
+            )
+            .await?;
+
+        println!("update installed");
+        app.restart();
+    }
+
+    Ok(())
 }
