82718c3b391cca290cb58d2c51f469a232f60a60
HVCsano <55344602+HVCsano@users.noreply.github.com>
2025-01-19 02:08:39 +0100

    app update done well

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,50 +1,18 @@
-use std::{thread, time::Duration};
-
 use tauri::Manager;
-use tauri_plugin_updater::UpdaterExt;
+
+#[tauri::command]
+async fn update_done(app_handle: tauri::AppHandle) {
+    let overlay = app_handle.get_webview_window("overlay").unwrap();
+    let loader = app_handle.get_webview_window("loader").unwrap();
+    loader.hide().unwrap();
+    overlay.show().unwrap();
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
-            let handle = app.handle().clone();
-            tauri::async_runtime::spawn(async move {
-                update(handle).await.unwrap();
-            });
-            let overlay = app.get_webview_window("overlay").unwrap();
-            let loader = app.get_webview_window("loader").unwrap();
-            let main = app.get_webview_window("main").unwrap();
-            loader.show().unwrap();
-            loader.set_focus().unwrap();
-            thread::sleep(Duration::from_secs(5));
-            loader.hide().unwrap();
-            overlay.show().unwrap();
-            Ok(())
-        })
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .invoke_handler(tauri::generate_handler![update_done])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
-    if let Some(update) = app.updater()?.check().await? {
-        let mut downloaded = 0;
-
-        // alternatively we could also call update.download() and update.install() separately
-        update
-            .download_and_install(
-                |chunk_length, content_length| {
-                    downloaded += chunk_length;
-                    println!("downloaded {downloaded} from {content_length:?}");
-                },
-                || {
-                    println!("download finished");
-                },
-            )
-            .await?;
-
-        println!("update installed");
-        app.restart();
-    }
-
-    Ok(())
 }
--- app/src/routes/loader/+page.svelte
+++ app/src/routes/loader/+page.svelte
@@ -1,9 +1,54 @@
+<script lang="ts">
+	import { check } from '@tauri-apps/plugin-updater';
+	import { invoke } from '@tauri-apps/api/core';
+	import { relaunch } from '@tauri-apps/plugin-process';
+	import { onMount } from 'svelte';
+	let text = $state('Frissítések keresése');
+	onMount(async () => {
+		setTimeout(async () => {
+			const update = await check();
+			if (update) {
+				text = 'Frissítés találva';
+				console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`);
+				let downloaded = 0;
+				let contentLength = 0;
+				// alternatively we could also call update.download() and update.install() separately
+				await update.downloadAndInstall((event) => {
+					switch (event.event) {
+						case 'Started':
+							contentLength = event.data.contentLength!;
+							console.log(`started downloading ${event.data.contentLength} bytes`);
+							break;
+						case 'Progress':
+							downloaded += event.data.chunkLength;
+							text = 'Letötés: ' + Math.round((downloaded / contentLength) * 100) + '%';
+							console.log(`downloaded ${downloaded} from ${contentLength}`);
+							break;
+						case 'Finished':
+							console.log('download finished');
+							text = 'Telepítés';
+							break;
+					}
+				});
+
+				console.log('update installed');
+				text = 'Frissítés kész, újraindítás';
+				await relaunch();
+			}
+			text = 'App indítása';
+			setTimeout(() => {
+				invoke('update_done');
+			}, 1000);
+		}, 1000);
+	});
+</script>
+
 <div
 	class="pointer-events-none selection:bg-none justify-center items-center text-center bg-gray-900 w-screen h-screen"
 >
 	<div class="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 		<img src="/favicon.png" class="w-[200px] m-auto" alt="" />
 		<h1 class="font-bold text-3xl text-white w-screen">SAMT App</h1>
-		<h2 class="text-gray-300 font-light">App indítása</h2>
+		<h2 class="text-gray-300 font-light">{text}</h2>
 	</div>
 </div>
