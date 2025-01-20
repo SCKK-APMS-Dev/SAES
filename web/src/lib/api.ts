713204815ad3665b45b34cd2b3cc158b0ca5fc50
HVCsano <55344602+HVCsano@users.noreply.github.com>
2025-01-20 16:48:23 +0100

    fixed snow

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,19 +1,21 @@
-export const apiUrl = process.env.NODE_ENV === "development"
-	? "http://localhost:3000"
-	: (process.env.api_prod_url as string);
-export const imageUrl = process.env.NODE_ENV === "development"
-	? "http://localhost:3100"
-	: (process.env.img_server_url as string);
+export const apiUrl =
+	process.env.NODE_ENV === 'development'
+		? 'http://localhost:3000'
+		: (process.env.api_prod_url as string);
+export const imageUrl =
+	process.env.NODE_ENV === 'development'
+		? 'http://localhost:3100'
+		: (process.env.img_server_url as string);
 
 let date = new Date();
 
 // * Hó engedélyezése Mikulás-naptól vízkeresztig
-export const snow = (date.getMonth() === 11 && date.getDate() >= 6) ||
-		(date.getMonth() < 2)
-	? true
-	: false;
+export const snow =
+	(date.getMonth() === 11 && date.getDate() >= 6) || (date.getMonth() < 2 && date.getDate() < 20)
+		? true
+		: false;
 
-export const christmas = (date.getMonth() === 11 && date.getDate() >= 6) ||
-		(date.getMonth() == 0 && date.getDate() <= 7)
-	? true
-	: false;
+export const christmas =
+	(date.getMonth() === 11 && date.getDate() >= 6) || (date.getMonth() == 0 && date.getDate() <= 7)
+		? true
+		: false;
