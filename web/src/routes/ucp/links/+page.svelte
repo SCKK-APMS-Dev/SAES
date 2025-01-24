ce6fcaea4f272a3036417177dc0220a203ae39e7
HVCsano <55344602+HVCsano@users.noreply.github.com>
2025-01-24 23:10:04 +0100

    added steam group

--- web/src/routes/ucp/links/+page.svelte
+++ web/src/routes/ucp/links/+page.svelte
@@ -6,7 +6,7 @@
 	<div class="m-auto">
 		<h1 class="font-itim mb-5 mt-5 text-4xl font-bold">Hasznos linkek</h1>
 		<div class="flex w-screen items-center justify-center gap-5">
-			{#if !data.layout.am}
+			{#if data.faction === 'SCKK'}
 				<a
 					href="/s/app"
 					target="_blank"
@@ -29,6 +29,16 @@
 					következő montage videóban is benne lehetsz!
 				</h2>
 			</a>
+			{#if data.faction === 'SCKK' || data.faction === 'TOW'}
+				<a
+					href="/s/steam"
+					target="_blank"
+					class="col-span-2 w-1/2 rounded-3xl border-4 border-[#1a9fff] p-5 transition-colors duration-500 hover:bg-[#1a9fff] lg:w-1/4"
+				>
+					<h1 class="text-2xl font-bold">SCKK Steam csoport</h1>
+					<h2 class="font-itim">Miért van Steam csoport? Miért ne legyen? Csak csatlakozz! 😉</h2>
+				</a>
+			{/if}
 		</div>
 	</div>
 </div>
