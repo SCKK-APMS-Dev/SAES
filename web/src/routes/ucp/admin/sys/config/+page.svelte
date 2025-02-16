6f0282f3fd8cf885182414b5ae2c82fba009a573
HVCsano <55344602+HVCsano@users.noreply.github.com>
2025-02-16 14:16:26 +0100

    qol change

--- web/src/routes/ucp/admin/sys/config/+page.svelte
+++ web/src/routes/ucp/admin/sys/config/+page.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
 	import { Factions } from '$lib/permissions';
+	import { Tooltip } from 'flowbite-svelte';
+	import { Button } from 'flowbite-svelte';
 
 	let { data } = $props();
 
-	let announcement = $state(data.config?.global.announcement);
-	let maintenance = $state(data.config?.global.maintenance);
+	let announcement = $state(
+		data.config?.global.announcement ? data.config?.global.announcement : null
+	);
+	let maintenance = $state(
+		data.config?.global.maintenance ? data.config?.global.maintenance : null
+	);
+
+	const handleChange = () => {
+		if (announcement === '') {
+			announcement = null;
+		}
+		if (maintenance === '') {
+			maintenance = null;
+		}
+	};
 </script>
 
 <div class="m-5 grid grid-cols-2 gap-2 text-center text-white">
@@ -16,8 +31,15 @@
 			type="text"
 			placeholder={!announcement ? 'nincs beállítva' : ''}
 			name="announcement"
+			onchange={handleChange}
 			bind:value={announcement}
 		/>
+		{#if announcement !== data.config?.global.announcement}
+			<Button
+				class="icon-[material-symbols--save-as] h-6 w-6 rounded-xl bg-white font-bold transition-all duration-150 hover:bg-slate-500"
+			></Button>
+			<Tooltip>Változás mentése</Tooltip>
+		{/if}
 		<label for="maintenance" class="text-xl font-bold">Karbantartás: </label>
 		<input
 			type="text"
@@ -25,6 +47,13 @@
 			name="maintenance"
 			placeholder={!maintenance ? 'nincs beállítva' : ''}
 			bind:value={maintenance}
+			onchange={handleChange}
 		/>
+		{#if maintenance !== data.config?.global.maintenance}
+			<Button
+				class="icon-[material-symbols--save-as] h-6 w-6 rounded-xl bg-white font-bold transition-all duration-150 hover:bg-slate-500"
+			></Button>
+			<Tooltip>Változás mentése</Tooltip>
+		{/if}
 	</div>
 </div>
